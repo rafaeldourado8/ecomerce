66ad15b67b28383425bcb173744e882ad362292b
Rafael Dourado <raffadrugs@gmail.com>
2025-10-24 23:11:00 +0000

    feat: ajusta configuração do banco de dados para ler variáveis de ambiente e adiciona verificação de saúde ao serviço Postgres

--- backend/app/config.py
+++ backend/app/config.py
@@ -2,7 +2,7 @@ import os
 
 APP_ENV = os.getenv('APP_ENV', 'devlopment')
 DATABASE_USERNAME = os.getenv('DATABASE_USERNAME', 'postgres')
-DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD', 'ameixa123')
-DATABASE_HOST = os.getenv('DATABASE_HOST', 'localhost')
+DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD')
+DATABASE_HOST = os.getenv('DATABASE_HOST')
 DATABASE_NAME = os.getenv('DATABASE_NAME', 'ecomerce')
 TEST_DATABASE_NAME = os.getenv('DATABASE_NAME', 'ecomerce_test')
